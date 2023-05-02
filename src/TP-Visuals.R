2e6c6afe32f6713c9287411d4766ecd406be0fed
Felipe <u202110902@upc.edu.pe>
2023-05-02 12:39:32 -0500

    Se modifico la Visualizacion de afluencia y paises por hotel, se agrego la Visualizacion por Overbooking por hotel

--- src/TP-Visuals.R
+++ src/TP-Visuals.R
@@ -21,50 +21,86 @@ datos$agent <- NULL
 datos$reservation_status <- NULL
 datos$reservation_status_date <- NULL
 
-# Visualizamos los nuevos datos
+# Visualizamos los paises por hotel
 
-## Visualizacion paises
-paises = get_paises(datos,country)
-paises = as.character(datos[["country"]])
-paises <- sort(unique(datos$country))
 
-## Limpieza paises top
-paises_top <- table(datos$country)
-paises_top <- paises_top[paises_top > 1000]
-paises_n <- names(paises_top)
+## Creacion de tabla con columnas Hotel y Pais 
+
+paises_top <- table(hotel = datos$hotel, country = datos$country )
+
+## Filtrado de los datos
+
+paises_filter <- paises_top[,(paises_top[2,] > 1000 & paises_top[1,] > 1000)]
+paises_filter
+paises_names <- names(paises_filter[1,])
+paises_names
 
 ## Visualizacion paises top
-barplot(paises_top,main="Paises del Mundo Top",names = c(paises_n))
+barplot(paises_filter,main="Paises del Mundo Top",names = c(paises_names),legend=c("Resort","City"), ylab = "Frecuencia", xlab = "Paises")
+
+
 
 # Visualizacion afluencia en fines de semana y dias de semana por tipo de hotel
 
 ## Instalamos la libreria necesaria
 library(ggplot2)
+library(reshape2)
+library(scales)
 
-# Extraemos la data
+## Extraemos la data
 datos.hotel.stay <- datos[, c("hotel", "stays_in_week_nights", "stays_in_weekend_nights")]
 
-# Convertir las variables numéricas a factor
+View(datos.hotel.stay)
+## Convertir las variables numéricas a factor
 datos.hotel.stay$hotel <- as.factor(datos.hotel.stay$hotel)
-datos.hotel.stay$stays_in_week_nights <- as.factor(datos.hotel.stay$stays_in_week_nights)
-datos.hotel.stay$stays_in_weekend_nights <- as.factor(datos.hotel.stay$stays_in_weekend_nights)
 
-# Convertir los datos a formato "largo" para poder usarlos con ggplot2
+summary(datos.hotel.stay)
+## Convertir los datos a formato "largo" para poder usarlos con ggplot2
 df_sum <- aggregate(cbind(stays_in_week_nights, stays_in_weekend_nights) ~ hotel, data = datos.hotel.stay, FUN = sum)
 
-# Convertir los datos a formato "largo" para poder usarlos con ggplot2
-df_long <- reshape2::melt(df_sum, id.vars = "hotel")
+## Convertir los datos a formato "largo" para poder usarlos con ggplot2
+df_long <- melt(df_sum, id.vars = "hotel")
+complete.cases(df_long)
 
+## Visualizamos la data
 
-# Visualizamos la data
-
-ggplot(df_long, aes(x = hotel, y = value, fill = variable)) +
+ggplot(df_long,aes(x = hotel, y = value, fill = variable, y_max= 10000, y_seq=1000)) +
   geom_bar(stat = "identity", position = "stack") +
+  scale_y_continuous(labels = comma_format()) +
   xlab("Hotel") +
   ylab("Cantidad de noches de estancia") +
   ggtitle("Comparación de noches de estancia en días de semana y fines de semana por hotel")
 
+porcentaje_paises <- prop.table(table(datos$country))
+porcentaje_paises_ordenado_desc <- head(round(sort(porcentaje_paises, decreasing = TRUE), 3), 10)
+porcentaje_paises_ordenado_desc <- porcentaje_paises_ordenado_desc * 100
 
+
+# Numero de veces que la habitacion asignada fue diferente a la reservada por hotel y por mes y año(minimizar casos de
+
+datos.booking <- datos[, c("hotel", "arrival_date_year", "arrival_date_month",
+                              "reserved_room_type","assigned_room_type")]
+
+#Creacion de una tabla Overbooking
+datos.booking$overbooking <- ifelse(datos.booking$reserved_room_type == datos.booking$assigned_room_type, "No", "Si")
+
+# Factor de las columnas
+datos.booking$hotel <- factor(datos.booking$hotel)
+datos.booking$arrival_date_year <- factor(datos.booking$arrival_date_year)
+datos.booking$arrival_date_month <- factor(datos.booking$arrival_date_month)
+datos.booking$reserved_room_type <- factor(datos.booking$reserved_room_type)
+datos.booking$assigned_room_type <- factor(datos.booking$assigned_room_type)
+datos.booking$overbooking <- factor(datos.booking$overcooking)
+
+#Filtrado de columnas
+
+datos.booking.filter.year <- datos.booking[, c("hotel", "arrival_date_year","overbooking")]
+
+View(datos.booking.filter.year)
+
+## Filtrado de los datos
+
+datos.booking <- paises_top[,(paises_top[2,] > 1000 & paises_top[1,] > 1000)]
 
 # Separamos la tabla en dos tipos de hotel
 
