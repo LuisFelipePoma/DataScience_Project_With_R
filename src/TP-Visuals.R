3d769fef94dbfd76566a3887337e56dcd977afc6
Felipe <u202110902@upc.edu.pe>
2023-05-02 00:30:34 -0500

    Visualizacion de comparacion de estadia entre fines y entresemana por hotel

--- src/TP-Visuals.R
+++ src/TP-Visuals.R
@@ -1,4 +1,5 @@
 setwd("C:\\Users\\pms_l\\OneDrive\\Escritorio\\uni\\Introduccion a Data Science\\Trabajo-Parcial-de-Data-Science\\src")
+rm(list = ls())
 
 datos <- read.csv("hotel_bookings/hotel_bookings.csv", sep = ",")
 View(datos)
@@ -22,28 +23,52 @@ datos$reservation_status_date <- NULL
 
 # Visualizamos los nuevos datos
 
-# Visualizacion paises
-
+## Visualizacion paises
 paises = get_paises(datos,country)
 paises = as.character(datos[["country"]])
 paises <- sort(unique(datos$country))
 
-#Limpieza paises top
-
+## Limpieza paises top
 paises_top <- table(datos$country)
-paises_top <- paises_n[paises_n > 1000]
+paises_top <- paises_top[paises_top > 1000]
 paises_n <- names(paises_top)
 
-
+## Visualizacion paises top
 barplot(paises_top,main="Paises del Mundo Top",names = c(paises_n))
 
-boxplot(datos$adr, col = "deepskyblue", main= "Boxplot VEntas", horizontal = TRUE)
-boxplot(datos$adr ~datos$lead_time ,col="deepskyblue",main ="Histograma de ventas",xlab = "tiendas",ylab = "ventas $")
+# Visualizacion afluencia en fines de semana y dias de semana por tipo de hotel
+
+## Instalamos la libreria necesaria
+library(ggplot2)
+
+# Extraemos la data
+datos.hotel.stay <- datos[, c("hotel", "stays_in_week_nights", "stays_in_weekend_nights")]
+
+# Convertir las variables numéricas a factor
+datos.hotel.stay$hotel <- as.factor(datos.hotel.stay$hotel)
+datos.hotel.stay$stays_in_week_nights <- as.factor(datos.hotel.stay$stays_in_week_nights)
+datos.hotel.stay$stays_in_weekend_nights <- as.factor(datos.hotel.stay$stays_in_weekend_nights)
+
+# Convertir los datos a formato "largo" para poder usarlos con ggplot2
+df_sum <- aggregate(cbind(stays_in_week_nights, stays_in_weekend_nights) ~ hotel, data = datos.hotel.stay, FUN = sum)
+
+# Convertir los datos a formato "largo" para poder usarlos con ggplot2
+df_long <- reshape2::melt(df_sum, id.vars = "hotel")
+
+
+# Visualizamos la data
+
+ggplot(df_long, aes(x = hotel, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "stack") +
+  xlab("Hotel") +
+  ylab("Cantidad de noches de estancia") +
+  ggtitle("Comparación de noches de estancia en días de semana y fines de semana por hotel")
+
 
 
 # Separamos la tabla en dos tipos de hotel
 
-datos.resort <- datos[datos$hotel == "Resort Hotel",]
+datos.resort <- datos[datos$hotel == "Resort Hotel" ]
 datos.city <- datos[datos$hotel == "City Hotel",]
 
 View(datos.resort)
